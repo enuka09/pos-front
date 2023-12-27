7b416c834f3346d4fdc1335105ac148ca11c3e13
enuka09 <enukapinsara09@gmail.com>
2023-12-27 16:25:16 +0530

    Updated Customer view and added backend functionality

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,32 +1,70 @@
-function Customer() {
+import React, {useState} from "react";
+
+import axios from "axios";
+
+interface Customer {
+    id: string,
+    name: string,
+    address: string,
+    salary: number
+}
+
+const Customer: React.FC = () => {
+
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [salary, setSalary] = useState<number | ''>('');
+
+    const saveCustomer = async () => {
+        try {
+            const response = await axios.post('http://localhost:3000/api/v1/customers/create', {
+                name, address, salary
+            });
+            console.log(response)
+            setName('');
+            setAddress('');
+            setSalary('');
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+
     return (
         <>
             <br/>
             <div className="container">
+                <h1>Customer Details</h1>
+                <br/>
                 <div className="row">
                     <div className="col-12 col-sm-6 col-md-4">
                         <div className="form-group">
                             <label htmlFor="customerName" className="mb-1">Customer Name</label>
-                            <input type="text" className='form-control' id='customerName'/>
+                            <input value={name} onChange={(e) => {
+                                setName(e.target.value)
+                            }} type="text" className='form-control' id='customerName'/>
                         </div>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4">
                         <div className="form-group">
                             <label htmlFor="customerAddress" className="mb-1">Customer Address</label>
-                            <input type="text" className='form-control' id='customerAddress'/>
+                            <input value={address} onChange={(e) => {
+                                setAddress(e.target.value)}} type="text" className='form-control' id='customerAddress'/>
                         </div>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4">
                         <div className="form-group">
                             <label htmlFor="customerSalary" className="mb-1">Salary</label>
-                            <input type="text" className='form-control' id='customerSalary'/>
+                            <input value={salary} onChange={(e) => {
+                                setSalary(e.target.value == '' ? '' : parseFloat(e.target.value))
+                            }} type="number" className='form-control' id='customerSalary'/>
                         </div>
                     </div>
                 </div>
                 <br/>
                 <div className="row">
                     <div className="col-12">
-                        <button className='btn btn-primary col-12'>Save Customer</button>
+                        <button onClick={saveCustomer} className='btn btn-primary col-12'>Save Customer</button>
                     </div>
                 </div>
                 <hr/>
